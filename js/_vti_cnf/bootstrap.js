a71f243a309b9d52ba680e735e3d5251a981116a
BodyFatZero <goran.krstic@hotmail.com>
2015-04-01 19:43:08 +0200

    Imported all site files - first-time

---
+++ js/_vti_cnf/bootstrap.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|09 Mar 2015 10:40:26 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|PC\\ADMIN
+vti_modifiedby:SR|PC\\ADMIN
+vti_timecreated:TR|09 Mar 2015 10:40:26 -0000
+vti_cacheddtm:TX|09 Mar 2015 10:40:26 -0000
+vti_filesize:IR|66732
+vti_backlinkinfo:VX|
